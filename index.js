f1a9b755fe87271ecacbcebbbf105605c4a1e5a4
Tomasz Zawadzki <103272474+ExpertoMontanero@users.noreply.github.com>
2023-04-27 20:58:40 +0200

    Update index.js
    Solution specification

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function maxSumBetweenTwoNegatives(a) {
+  //variables for actuall sum and maxSum
   var sum = 0;
   var maxSum = 0;
+  //iterating and summing loop
   for (var index = 0; index < a.length; index++) {
     sum = 0;
     if (a[index] >= 0 && a[index - 1] < 0) {
       do {
+        //temporary suming
         sum = sum + a[index];
         index++;
       }
+      //condition to stop summing 
       while (a[index] >= 0);
       if (a[index] < 0)
         if (maxSum < sum) {
@@ -15,15 +19,20 @@ function maxSumBetweenTwoNegatives(a) {
         }
     }
   }
+  //if summ is 0
   if (maxSum == 0) {
     for (var x = 0; x < a.length; x++) {
+      //checking if it s because of 0 between minus numbers or lack of pair of minus numbers in array
       if (a[x] < 0 && a[x + 1] < 0) {
+        //returning maxSum as 0
         return maxSum;
       }
     }
+    //returning -1 if there is not at least 2 minus numbers in array
     return -1;
   }
   else {
+    //returning max sum if it is not 0
     return maxSum;
   }
 }
